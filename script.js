d545d769586cb00ed5224b70d442d2e54d4da203
Ayinla Kwamdeen <ayinlakwamdeen@Ayinlas-MacBook-Pro.local>
2023-02-05 19:50:49 +0000

    Randomize names commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const search = document.querySelector('.search')
+const btn = document.querySelector('.searchBtn')
+const input = document.querySelector('.input')
+
+btn.addEventListener('click', () => {
+  search.classList.toggle('active')
+  input.focus()
+})
